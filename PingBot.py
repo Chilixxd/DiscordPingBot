1732b79369e59fc9bb8decee4281168fc6a2a6ed
Chilixxd <59507848+Chilixxd@users.noreply.github.com>
2020-08-11 02:33:15 +0200

    Update PingBot.py

--- PingBot.py
+++ PingBot.py
@@ -26,6 +26,9 @@ Commands:
 
 -e = Everyone
 
+-c= Custom word to get spammed
+--custom = Custom word to get spammed
+
 Example: python3 PingBot.py -u
 ...
 Enter the username: discord 123
@@ -83,3 +86,51 @@ if '-e' in sys.argv:
          keyboard.release(Key.enter)
          keyboard.press(Key.enter)
          keyboard.release(Key.enter)
+
+if '--custom' in sys.argv: 
+   custom = input('Enter the word to get spammed: ')
+   pinged3 = int(input('How many times should the word/sentence get spammed?: '))
+   
+   print("[+] Made by Chili")
+   print("")
+   print("[+] Script Starting in 5 seconds")
+   time.sleep(1)
+   print("[+] Script Starting in 4 seconds") 
+   time.sleep(1)
+   print("[+] Script Starting in 3 seconds")
+   time.sleep(1)
+   print("[+] Script Starting in 2 seconds")
+   time.sleep(1)
+   print("[+] Script Starting in 1 second")
+
+
+   for x in range(pinged3):
+       keyboard.type(custom)
+       keyboard.press(Key.enter)
+       keyboard.release(Key.enter)
+       keyboard.press(Key.enter)
+       keyboard.release(Key.enter)
+
+if '-c' in sys.argv: 
+   custom = input('Enter the word to get spammed: ')
+   pinged3 = int(input('How many times should the word/sentence get spammed?: '))
+   
+   print("[+] Made by Chili")
+   print("")
+   print("[+] Script Starting in 5 seconds")
+   time.sleep(1)
+   print("[+] Script Starting in 4 seconds") 
+   time.sleep(1)
+   print("[+] Script Starting in 3 seconds")
+   time.sleep(1)
+   print("[+] Script Starting in 2 seconds")
+   time.sleep(1)
+   print("[+] Script Starting in 1 second")
+
+
+   for x in range(pinged3):
+       keyboard.type(custom)
+       keyboard.press(Key.enter)
+       keyboard.release(Key.enter)
+       keyboard.press(Key.enter)
+       keyboard.release(Key.enter)
